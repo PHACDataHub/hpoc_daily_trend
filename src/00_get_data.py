15f514faee7a4fd17e64fc364ec919c4ced47db5
Najmus Saqib <najmus.saqib@canada.ca>
2020-11-09 15:05:08 -0500

    Line spacing in Python file

--- src/00_get_data.py
+++ src/00_get_data.py
@@ -1,8 +1,9 @@
-# Import the hospitalization data from the scrapped file
 import pandas
 import glob
 import os
 import re
+
+# Import the hospitalization data from the scrapped file
 
 ## Read the hospitalization data
 pt_hosp_raw = pandas.read_excel('Y:\PHAC\IDPCB\CIRID\VIPS-SAR\EMERGENCY PREPAREDNESS AND RESPONSE HC4\EMERGENCY EVENT\WUHAN UNKNOWN PNEU - 2020\DATA AND ANALYSIS\Web Scraping\Trend analysis\COVID-19 historical archive_CURRENT.xlsx', 
