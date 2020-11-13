6b412b35d39260f44a5810a28051b9db341a87cc
Najmus Saqib <najmus.saqib@canada.ca>
2020-11-13 09:16:52 -0500

    Contributes to #2. Reading in SALT data from Y drive. Reading csv through R and xlsx through Python. The latter is several seconds faster but commented out for now. Can modify this if API or Selenium method for SALT is figured out.

--- src/00_get_data.py
+++ src/00_get_data.py
@@ -18,3 +18,7 @@ pt_icu_raw = pandas.read_excel('Y:\PHAC\IDPCB\CIRID\VIPS-SAR\EMERGENCY PREPAREDN
 # Import the all cases data
 list_of_files = glob.glob('Y:\PHAC\IDPCB\CIRID\VIPS-SAR\EMERGENCY PREPAREDNESS AND RESPONSE HC4\EMERGENCY EVENT\WUHAN UNKNOWN PNEU - 2020\DATA AND ANALYSIS\SAS_Analysis\Domestic data\*.rds')
 latest_file = max(list_of_files, key=os.path.getmtime)
+
+# Import the SALT lab data
+#salt_raw = pandas.read_excel('Y:\PHAC\IDPCB\CIRID\VIPS-SAR\EMERGENCY PREPAREDNESS AND RESPONSE HC4\EMERGENCY EVENT\WUHAN UNKNOWN PNEU - 2020\EPI SUMMARY\Trend analysis\_Current\_Source Data\SALT\Submitted+Reports.xlsx') 
+                    
