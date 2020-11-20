0a96a836d9eb779a404caabc8543e55748d7c704
islafar <73712802+islafar@users.noreply.github.com>
2020-11-20 14:52:35 -0500

    Added Canada, reordered jurisdictions in table

--- src/05_hosp_slides.R
+++ src/05_hosp_slides.R
@@ -43,7 +43,13 @@ ggplot(pt_hosp_icu_filter %>% filter(), aes(date, cases, colour = type)) +
     )
 
 #Create table for hospitalization metrics
-hosp_metrics1 <- pt_hosp_icu_filter %>%
+pt_hosp_icu_filter2 <- pt_hosp_icu %>%
+  filter(prname %in% c(jurisdiction,"Canada")) %>%
+  filter(date >= "2020-07-01") %>%
+  group_by(prname) %>%
+  mutate(label = if_else(date == max(date), as.character(round(cases, digits = 1)), NA_character_))
+
+hosp_metrics1 <- pt_hosp_icu_filter2 %>%
     filter(type=="hospitalized") %>%
     group_by(prname) %>%
     mutate(hosp7ma=round(rollmean(cases, k=7, fill=NA, align="right"))) %>%
@@ -53,7 +59,7 @@ hosp_metrics1 <- pt_hosp_icu_filter %>%
     rename("Jurisdiction"=prname, "Date"=date, "Hospitalizations"=cases, 
            "7 Day MA of Hospitalizations"=hosp7ma, "Weekly Change in Hospitalizations"=delta7)
 
-hosp_metrics2 <- pt_hosp_icu_filter %>%
+hosp_metrics2 <- pt_hosp_icu_filter2 %>%
     filter(type=="icu") %>%
     group_by(prname) %>%
     mutate(icu7ma=round(rollmean(cases, k=7, fill=NA, align="right"))) %>%
@@ -64,6 +70,11 @@ hosp_metrics2 <- pt_hosp_icu_filter %>%
            "Weekly Change in ICU"=delta7)
 
 Hosp_Metrics <- hosp_metrics1 %>%
-    left_join(hosp_metrics2, by=c("Jurisdiction","Date"))
+    left_join(hosp_metrics2, by=c("Jurisdiction","Date")) %>%
+    filter(Date==max(Date))
+
+metricorder <- c("Canada","British Columbia","Alberta","Ontario","Quebec","Manitoba","Saskatchewan")
+
+Hosp_Metrics <- Hosp_Metrics %>% slice(match(metricorder,Jurisdiction))
 
 remove(hosp_metrics1,hosp_metrics2)
